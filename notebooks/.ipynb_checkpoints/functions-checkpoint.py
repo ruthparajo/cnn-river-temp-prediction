2e55e49af97892ed27346a32dcc3ec293e04abca
Parajó Ruth <ruth.parajo@oceanus.einet.ad.eivd.ch>
2024-10-07 15:52:32 +0200

    prova2

--- notebooks/.ipynb_checkpoints/functions-checkpoint.py
+++ notebooks/.ipynb_checkpoints/functions-checkpoint.py
@@ -1,3 +1,14 @@
+import subprocess
+# Define the path to the install_dependencies.sh script
+script_path = '../install_dependencies.sh'
+
+# Run the shell script
+try:
+    subprocess.run(['bash', script_path], check=True)
+    print("Dependencies installed successfully.")
+except subprocess.CalledProcessError as e:
+    print(f"Error occurred while running the script: {e}")
+
 import rasterio
 import pandas as pd
 import os
