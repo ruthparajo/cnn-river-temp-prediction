0be6f33ba163a98aa0aa1c9451f155bab4161282
ruthparajo <118297984+ruthparajo@users.noreply.github.com>
2024-10-22 13:56:47 +0200

    c

---
+++ notebooks/test_earthengine.py
@@ -0,0 +1,5 @@
+import ee
+print("Earth Engine API successfully imported")
+ee.Authenticate()  # Solo si es la primera vez que te autenticas
+ee.Initialize()
+print("Earth Engine API successfully initialized")
